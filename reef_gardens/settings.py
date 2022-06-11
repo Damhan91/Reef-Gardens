912c6d2eba39bde3b3f1b78cb8b1f2ffc80aea73
Damhan91 <damhanp@gmail.com>
2022-06-11 14:39:46 +0000

    removed secret key and changed debug

--- reef_gardens/settings.py
+++ reef_gardens/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kjhvbx6x)4b(+dl(sks=fvn5(ra9!4v1+#cca%zstmw*9zi-#l'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['reef-gardens.herokuapp.com', 'localhost']
 
