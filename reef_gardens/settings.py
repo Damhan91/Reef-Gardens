6f56d8c85a8c2cdfc15509a229e4d810b4850588
Damhan91 <damhanp@gmail.com>
2022-06-11 16:50:57 +0000

    added secret key

--- reef_gardens/settings.py
+++ reef_gardens/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '9^z^5uw)9r^@v08e!7(gpr^@e*9(b-j-)69nk)t&-go+!9ab$q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
