96042d6f5703cd15f44775edf1426e48dd59d5d8
Damhan91 <damhanp@gmail.com>
2022-06-28 22:41:12 +0000

    removed nav item in main-nav

--- reef_gardens/settings.py
+++ reef_gardens/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',    
+    'allauth.socialaccount',
     'home',
     'products',
     'bag',
