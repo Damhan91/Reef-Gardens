fa89b51ee74f2fa77a1fa900cff47ee83c0f5462
Damhan91 <damhanp@gmail.com>
2022-06-29 01:57:04 +0000

    made edits to admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from . models import Blogs
 
 
-admin.site.register(Blogs)
+class BlogsAdmin(admin.ModelAdmin):
+    """ Admin view for Blog Posts """
+    list_display = ('title', 'created', 'updated')
+    search_fields = ('title', 'body')
+    list_filter = ('title', 'created')
