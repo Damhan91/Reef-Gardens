3489bf4f3ba8d3395ac7e380eae5a80bb56c9693
Damhan91 <damhanp@gmail.com>
2022-06-29 00:29:47 +0000

    added url to main app

--- reef_gardens/urls.py
+++ reef_gardens/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('about/', include('about.urls')),    
+    path('about/', include('about.urls')),
+    path('blog/', include('blog.urls')),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'reef_gardens.views.handler404'
