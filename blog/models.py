cc7fb4099a3b0c09723df4a514f7252d68c5446a
Damhan91 <damhanp@gmail.com>
2022-06-29 01:26:30 +0000

    changed name on models

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
-class Blog(models.Model):
+class Blogs(models.Model):
 
     title = models.CharField(max_length=250)
     content = models.TextField(max_length=1000)
+    image = models.ImageField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
