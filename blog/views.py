d143dc8f74929e41d37eef34db4831712435f1f2
Damhan91 <damhanp@gmail.com>
2022-06-29 01:31:59 +0000

    added nav link and finished view for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Blogs
 # Create your views here.
+
+
+def view_blog(request):
+    """ View to render blog posts """
+
+    posts = Blogs.objects.all()
+
+    template = 'blog/blog.html'
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, template, context)
