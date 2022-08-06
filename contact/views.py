8db265cdb9404859093ed7a73a2bea670c9a8993
Damhan91 <damhanp@gmail.com>
2022-08-06 10:50:58 +0000

    created content in view.py for  the disply of the contact us form and to handle the submissions

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact_us(request):
+    """
+    Display the contact us form and handle submission
+    """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thanks for your message!')
+
+        else:
+            form = ContactForm()
+            messages.warning(request, 'The Message not sent. Please try again.')
+
+    form = ContactForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
