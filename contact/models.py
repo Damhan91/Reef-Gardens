7cdb8c978b1ca7ab7daeac49ce86aaf340b3dd10
Damhan91 <damhanp@gmail.com>
2022-08-06 10:37:06 +0000

    added contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ Schema for the ContactUs model """
+
+    class Meta:
+        verbose_name_plural = 'Contact Us'
+
+    name = models.CharField(max_length=254)
+    email = models.EmailField()
+    subject = models.CharField(max_length=254)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.subject} from {self.name}"
